85488ee84f63377ef422f4e4d9fa7d450c3d995c
Shreya <shreyabatra003@gmail.com>
2023-10-14 20:19:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const {Data1, setData} = useState('')
+  const getData = () => {
+    axios.get('https://dummyjson.com/users')
+    .then(response => {
+      console.log(response.data.content)
+      setData(response.data.content)
+    }).catch(err => {
+      console.log(err)
+
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick = {getData}>Get Data</button>
+      {Data1 && <p>{Data1}</p> }
     </div>
   );
 }
